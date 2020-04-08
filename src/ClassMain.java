eb39c705472680adc5ef28fcba994a4f47438589
Cara7 <tahiana.carhelle.ratsimba@esti.mg>
2020-04-08 12:36:28 +0300

    this

--- src/ClassMain.java
+++ src/ClassMain.java
@@ -2,10 +2,15 @@
 public class ClassMain {
 
 	public static void main(String[] args) {
-		Ville villeInconnu = new Ville();
-		Ville ville1 = new Ville("Marseille", 12000, "France");
-		ville1.nomVille = "haha";
-		System.out.println(ville1.nomVille)
+		Ville v = new Ville();
+		Ville v1 = new Ville("Marseille", 1236, "France");       
+		Ville v2 = new Ville("Rio", 321654, "Brésil");
+		        
+		System.out.println("\n\n"+v1.decrisToi());
+		System.out.println(v.decrisToi());
+		System.out.println(v2.decrisToi()+"\n\n");
+		System.out.println(v1.comparer(v2));
+		
 	}
 
 }
--- src/Ville.java
+++ src/Ville.java
@@ -1,8 +1,10 @@
 
 public class Ville {
-	String nomVille;
-	int nbHabitants;
-	String nomPays;
+	private String nomVille;
+	private int nbHabitants;
+	private String nomPays;
+	private char categorie;
+	
 	
 	//Constructeur par défaut
 	public Ville() {
@@ -10,14 +12,81 @@ public class Ville {
 		nomVille = "Inconnu";
 		nomPays = "Inconnu";
 		nbHabitants = 0;
+		this.setCategorie();
 	}
 	
 	//Constructeur avec param
-	public Ville(String nomVille, int nbHabitants, String nomPays) {
+	public Ville(String pNomVille, int pNbHabitants, String pNomPays) {
 		System.out.println("Création d'une ville avec des paramètres !");
-		nomVille = nomVille;
-		nbHabitants = nbHabitants;
-		nomPays = nomPays;
-
+		nomVille = pNomVille;
+		nbHabitants = pNbHabitants;
+		nomPays = pNomPays;
+		this.setCategorie();
 	}
+	
+	//Accesseurs
+	public String getNomVille() {
+		return nomVille;
+	}
+	
+	public int getNbHabitants() {
+		return nbHabitants;
+	}
+	
+	public String getNomPays() {
+		return nomPays;
+	}
+	
+	public char getCategorie() {
+		return categorie;
+	}
+	
+	//Mutateurs
+	public void setNomville(String nVille) {
+		nomVille = nVille;
+	}
+	
+	public void setNbHabitants(int nbHab) {
+		nbHabitants = nbHab;
+	}
+	
+	public void setNomPays(String nPays) {
+		nomPays = nPays;
+	}
+	
+	//Définit la catégorie de la ville
+	private void setCategorie() {
+		 int bornesSuperieures[] = {0, 1000, 10000, 100000, 500000,
+				 1000000, 5000000, 10000000};
+		 char categories[] = {'?', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		 'H'};
+		 int i = 0;
+		    while (i < bornesSuperieures.length && this.nbHabitants >
+		bornesSuperieures[i])
+		      i++;
+		    this.categorie = categories[i];
+	}
+	
+	//Décrit la ville
+	  public String decrisToi() {
+	    return "\t"+this.nomVille+" est une ville de "+this.nomPays+ ","
+	    		+ "	elle comporte : "+this.nbHabitants+" habitant(s) => elle est donc"
+	    				+ "de catégorie : "+this.categorie;
+	  }
+	  
+	  //Retourne une chaîne de caractères selon le résultat de la comparaison
+	    public String comparer(Ville v1) {
+	      String str = new String();
+	      if (v1.getNbHabitants() > this.nbHabitants)
+	        str = v1.getNomVille()+" est une ville plus peuplée que"
+	        		+ " "+this.nomVille;
+	       
+	      else
+	        str = this.nomVille+" est une ville plus peuplée que  "+v1.getNomVille();
+	       
+	      return str;
+	    }
+	
+	
+	
 }
